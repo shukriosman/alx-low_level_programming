334eb4f255d4a8759e47523445eb7ab72a715031
shukriosman <osmanshukri97@gmail.com>
2022-09-13 07:56:27 -0700

    0-putcher.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -2,8 +2,8 @@
 
 /**
  *_putcher - writes the character c to stdout
- @c: The character to print
-
+ *@c: The character to print
+ *
  *Return: on sucess 1.
  *On error, -1 is returned, and error is set appropriately.
  */
