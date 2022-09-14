a04e1271eab900ea204d90a93bc143276db6f217
shukriosman <osmanshukri97@gmail.com>
2022-09-14 07:52:08 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
  *  print_alphabet - Print alphabet in lowercase using only `_putchar`
  *  Return: 0
  */
-void print_alphabet(main);
+void print_alphabet(main)
 {
 	char c = 'a';
 
@@ -12,5 +12,5 @@ void print_alphabet(main);
 	         print_alphabet(main);
 		c++;
 	}
-	    print_alphabet(main)
+	    print_alphabet(main);
 }
