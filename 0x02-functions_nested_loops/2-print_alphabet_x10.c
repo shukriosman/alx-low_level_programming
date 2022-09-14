6d855fdc7d4aa427b186ee2cfaf1d4d983b54daf
shukriosman <osmanshukri97@gmail.com>
2022-09-14 11:00:45 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,8 @@
 /**
  * print_alphabet_x10 - Print alphabet in lowercase 10 times
  * Return: 0
- void print_alphabet_x10(void)
+ */
+ void print_alphabet_x10(void);
  {
        	 int i = 0;
 	 char c;
