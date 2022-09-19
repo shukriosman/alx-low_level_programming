7f0b36ce3a30843eb5df65cdf1b3a3d3f9e7fcd4
shukriosman <osmanshukri97@gmail.com>
2022-09-19 07:08:33 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,17 +1,23 @@
 #include "main.h"
 
 /**
- * swap_int - functions that swaps the value of two integers.
- * @a: this is the first entry
- * @b: this is the second entry
- *
- *
+ * _strncat - concatenate two strings
+ * @dest: string
+ * @src: string
+ * @n: number of elements to concatenate in
+ * Return: pointer to resulting `dest`
  */
-void swap_int(int *a, int *b);
-{
-	int changed;
 
-	changed = *a;
-	*a = *b;
-	*b = changed;
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, c;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	for (c = 0; src[c] != '\0' && n > 0; c++, n--, i++)
+	{
+		dest[i] = src[c];
+	}
+	return (dest);
 }
