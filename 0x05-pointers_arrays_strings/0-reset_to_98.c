22b77534b2c69a5e20ca476eafdd8d32a4ab748c
shukriosman <osmanshukri97@gmail.com>
2022-09-20 11:18:31 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,25 +1,11 @@
 #include "main.h"
 
 /**
- * _strcat - concatenate two strings
- * @dest: char string to concatenate to
- * @src: char string
- * Return: pointer to resulting string `dest`
- */
+ *  * reset_to_98 - Set pointer param to 98.
+ *   * @n: int type pointer
+ *    */
 
-char *_strcat(char *dest, char *src)
+void reset_to_98(int *n)
 {
-	int i, c;
-
-	for (i = 0; dest[i] != '\0'; i++)
-		;
-
-	for (c = 0; src[c] != '\0'; c++)
-	{
-		dest[i] = src[c];
-		i++;
-	}
-
-	dest[i] = '\0';
-	return (dest);
+		*n = 98;
 }
